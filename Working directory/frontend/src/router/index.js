a126139bdc8b83a1b7285cbecbb2558c38e63f1f
erikssonphil <98100748+erikssonphil@users.noreply.github.com>
2023-02-23 08:49:14 -0600

    Add files via upload

---
+++ Working directory/frontend/src/router/index.js
@@ -0,0 +1,63 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// make all paths and names lowercase for consistency
+const routes = [
+  {
+    path: '/',
+    props: true,
+    component: () => import('../components/homePage.vue')
+  },
+  {
+    path: '/intakeform',
+    name: 'intakeform',
+    props: true,
+    component: () => import('../components/intakeForm.vue')
+  },
+  {
+    path: '/findclient',
+    name: 'findclient',
+    component: () => import('../components/findClient.vue')
+  },
+  {
+    path: '/updateclient/:id',
+    name: 'updateclient',
+    props: true,
+    component: () => import('../components/updateClient.vue')
+  },
+  {
+    path: '/eventform',
+    name: 'eventform',
+    component: () => import('../components/eventForm.vue')
+  },
+  {
+    path: '/serviceform',
+    name: 'serviceform',
+    component: () => import('../components/serviceForm.vue')
+  },
+  {
+    path: '/findevents',
+    name: 'findevents',
+    component: () => import('../components/findEvents.vue')
+  },
+  {
+    path: '/eventdetails/:id',
+    name: 'eventdetails',
+    props: true,
+    component: () => import('../components/eventDetails.vue')
+  },
+  {
+    path: '/login',
+    name: 'loginpage',
+    component: () => import('../components/Login.vue')
+  },
+  {
+    path: '/findservice',
+    name: 'findservice',
+    component: () => import('../components/findService.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+export default router
