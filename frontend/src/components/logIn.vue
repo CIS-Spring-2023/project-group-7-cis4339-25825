927ac6160c57d8708300084dbefdc4ca4e6b7eb8
snblackb <98061855+snblackb@users.noreply.github.com>
2023-02-23 16:24:16 -0600

    Update logIn.vue

--- frontend/src/components/logIn.vue
+++ frontend/src/components/logIn.vue
@@ -4,3 +4,13 @@ import { required, email, alpha, numeric } from '@vuelidate/validators'
 import axios from 'axios'
 const apiURL = import.meta.env.VITE_ROOT_API
 </script>
+
+<template>
+  <main>
+    <div>
+      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
+        Login
+      </h1>
+    </div>
+  </main>
+</template>
