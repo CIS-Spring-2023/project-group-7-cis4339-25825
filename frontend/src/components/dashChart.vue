052e13752b7ef2b68d6fda85f0b871d8e606563b
snblackb <98061855+snblackb@users.noreply.github.com>
2023-03-05 15:42:14 -0600

    Create dashChart.vue
    Added component file for dashboard pie chart

---
+++ frontend/src/components/dashChart.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <canvas id="dash-pie-chart"></canvas>
+    </div>
+  </template>
+  
+  <script>
+  import { Chart, registerables } from 'chart.js'
+  import ChartData from '../assets/ChartData.js'
+  Chart.register(...registerables);
+  export default {
+    name: 'DataChart',
+    data() {
+      return {
+        ChartData: ChartData
+      }
+    },
+    mounted() {
+      const ctx = document.getElementById('dash-pie-chart');
+      new Chart(ctx, this.ChartData);
+    }
+  }
+  </script>
+  
