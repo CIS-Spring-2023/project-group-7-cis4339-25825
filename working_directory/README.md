e29ba1eceef6d06e9a0d741a6f8ecca9526a5cf4
erikssonphil <98100748+erikssonphil@users.noreply.github.com>
2023-03-05 22:35:40 -0600

    Login - Routing - CRUD

--- working_directory/README.md
+++ working_directory/README.md
@@ -1,11 +1,20 @@
-Project Setup:
-npm install
+# Data Platform Frontend
 
-Project uses local JSON file for initial data:
-npm install -g json-server
+This project uses [VueJS 3](https://vuejs.org/) with [Vite](https://vitejs.dev/) and [TailwindCSS](https://tailwindcss.com/).
+For form validation [Vuelidate](https://vuelidate-next.netlify.app/) is used.
 
-Run local server to run data/db.json:
-json-server --watch data/db.json
+## Project setup
 
-Run development server:
-npm run dev
+    npm install
+
+### Before startup
+You need to either have a local backend running and or use a cloud deployed backend.
+
+Setup a .env file with following variables, e.g.:
+
+    VITE_ROOT_API=http://localhost:3000
+
+VITE_ROOT_API is the URL for the backend API, either on localhost or in the cloud.
+### Compiles and hot-reloads for development
+
+    npm run dev
---
+++ working_directory/src/Views/FindEvent.vue
@@ -0,0 +1,157 @@
+<template>
+    <main>
+      <div>
+        <h1
+          class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+        >
+          List of Events
+        </h1>
+      </div>
+      <div class="px-10 pt-20">
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+        >
+          <h2 class="text-2xl font-bold">Search Event By</h2>
+          <!-- Displays Client Name search field -->
+          <div class="flex flex-col">
+            <select
+              class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="searchBy"
+            >
+              <option value="Event Name">Event Name</option>
+              <option value="Event Date">Event Date</option>
+            </select>
+          </div>
+          <div class="flex flex-col" v-if="searchBy === 'Event Name'">
+            <label class="block">
+              <input
+                type="text"
+                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                v-model="name"
+                v-on:keyup.enter="handleSubmitForm"
+                placeholder="Enter event name"
+              />
+            </label>
+          </div>
+          <!-- Displays event date search field -->
+          <div class="flex flex-col" v-if="searchBy === 'Event Date'">
+            <input
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              type="date"
+              v-model="eventDate"
+              v-on:keyup.enter="handleSubmitForm"
+            />
+          </div>
+        </div>
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+        >
+          <div></div>
+          <div></div>
+          <div class="mt-5 grid-cols-2">
+            <button
+              class="mr-10 border border-red-700 bg-white text-red-700 rounded"
+              @click="clearSearch"
+              type="submit"
+            >
+              Clear Search
+            </button>
+            <button
+              class="bg-red-700 text-white rounded"
+              @click="handleSubmitForm"
+              type="submit"
+            >
+              Search Event
+            </button>
+          </div>
+        </div>
+      </div>
+  
+      <hr class="mt-10 mb-10" />
+      <!-- Display Found Data -->
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+      >
+        <div class="ml-10">
+          <h2 class="text-2xl font-bold">List of Events</h2>
+          <h3 class="italic">Click table row to edit/display an entry</h3>
+        </div>
+        <div class="flex flex-col col-span-2">
+          <table class="min-w-full shadow-md rounded">
+            <thead class="bg-gray-50 text-xl">
+              <tr>
+                <th class="p-4 text-left">Event Name</th>
+                <th class="p-4 text-left">Event Date</th>
+                <th class="p-4 text-left">Event Address</th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-300">
+              <tr
+                v-for="event in events"
+                :key="event._id"
+              >
+                <td class="p-2 text-left">{{ event.name }}</td>
+                <td class="p-2 text-left">{{ formattedDate(event.date) }}</td>
+                <td class="p-2 text-left">{{ event.address }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <p>{{ searchBy }}</p>
+      <p>{{ name }}</p>
+      <p>{{ eventDate }}</p>
+    </main>
+  </template>
+
+<script>
+import { DateTime } from 'luxon'
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+        events: [],
+      // Parameter for search to occur
+      searchBy: '',
+      name: '',
+      eventDate: ''
+    }
+  },
+  computed: {
+    ...mapState(['organizationId', 'organizationEvents'])
+  },
+  mounted() {
+    this.setEvents() 
+  },
+  methods: {
+    setEvents() {
+        this.events = this.organizationEvents
+    },
+    // better formattedDate
+    formattedDate(datetimeDB) {
+      const dt = DateTime.fromISO(datetimeDB, {
+        zone: 'utc'
+      })
+      return dt
+        .setZone(DateTime.now().zoneName, { keepLocalTime: true })
+        .toLocaleString()
+    },
+    handleSubmitForm() {
+        if (this.searchBy === 'Event Name') {
+            this.events = this.organizationEvents.filter((event) => event.name.toLowerCase().includes(this.name.toLowerCase()));
+        } 
+        else if (this.searchBy === 'Event Date') {
+            this.events = this.organizationEvents.filter((event) => event.date === this.eventDate);
+        }
+    },
+    clearSearch() {
+      // Resets all the variables
+      this.searchBy = ''
+      this.name = ''
+      this.eventDate = ''
+      this.setEvents()
+    }
+  }
+}
+</script>
